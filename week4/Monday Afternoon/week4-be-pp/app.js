1547ac34695644a79073d132a4f56ddb5f9b313d
YingLuo0208 <Ying.Luo@metropolia.fi>
2025-09-10 11:12:20 +0300

    Step 5

--- week4/Monday Afternoon/week4-be-pp/app.js
+++ week4/Monday Afternoon/week4-be-pp/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const morgan = require('morgan');
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 // Middleware to parse JSON
 app.use(express.json());
